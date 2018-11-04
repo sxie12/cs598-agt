3a6398cba4dbf22afa7035e9ce02ddbce71ae665
Shunping Xie <sxie12@illinois.edu>
2018-11-04 11:39:03 -0600

    working on simulation

--- compute_table.cpp
+++ compute_table.cpp
@@ -15,8 +15,59 @@ const int inf = 0x3f3f3f3f;
 const int mod = 1e9+7;
 const double eps = 1e-8; 
 
+// for each hand, play strat if H and if T
+
+struct strat {
+    string strats[2];
+};
+
+const int n[2] = {5, 9};
+const string p1s[n[0]] = {"", "", "", "", ""};
+const string p2s[n[1]] = {"", "", "", "", "", "", "", "", ""};
+const PII outcomes[4] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}}; // heads = 0, tails = 1
+
+int total[11][11];
+double ev[11][11];
+vector<string> v[2];
+vector<strat> htv[2]; // H/T strat for player i 
+
+// turn, player one strat, player two strat, player 1 flip, player 2 flip, pot
+void move(int turn, int p1a, int p2a, int p1f, int p2f, int pot) {
+    // 0 = fold, 1 = check, 2 = raise
+    if (turn & 1) {
+        // player 1
+    } else {
+        // player 2
+    }
+}
+
+void set_strats(int player) {
+    for (int i = 0; i < n[player]; ++i)
+        for (int j = 0; j < n[player]; ++j)
+            htv[player].push_back(strat{{v[player][i], v[player][j]}});
+}
+
+void get_ev() {
+    for (int i = 0; i < n[0]; ++i)
+        for (int j = 0; j < n[1]; ++j)
+            ev[i][j] = (double) total[i][j] / 4.0;
+}
 
 int main() { _
+    for (int i = 0; i < n[0]; ++i)
+        v[0].push_back(p1s[i]);
+    for (int i = 0; i < n[1]; ++i)
+        v[1].push_back(p2s[i]);
+
+    set_strats(0);
+    set_strats(1);
+
+    for (int i = 0; i < htv[0].size(); ++i)
+        for (int j = 0; j < htv[1].size(); ++j)
+            for (int k = 0; k < 4; ++k)
+                move(1, i, j, outcomes[k].x, outcomes[k].y, 2);
+
+    get_ev();
 
     return 0;
 }
